09cc2cc766b47ddd8b71ec8c8bb1931c57fdfade
Nandini Tibrewal <nandini.twl24@gmail.com>
2025-11-16 23:28:02 +0530

    Updated navbar title

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Create Your Own Favorite Watchlist</Link>
+        <Link to="/">Movie WatchList</Link>
       </div>
 
       <div className="navbar-links">
